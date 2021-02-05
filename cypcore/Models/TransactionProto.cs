dc832770e662971ee5409e8b4c6427c1f72d4f0d
pingpongsneak <matthew.hellyer@pm.me>
2021-02-05 23:28:23 +0000

    refactor: transaction validation

--- cypcore/Models/TransactionProto.cs
+++ cypcore/Models/TransactionProto.cs
@@ -3,9 +3,11 @@
 
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+
 using Newtonsoft.Json;
 
 using ProtoBuf;
+
 using CYPCore.Extentions;
 
 namespace CYPCore.Models
@@ -40,7 +42,7 @@ namespace CYPCore.Models
             {
                 results.Add(new ValidationResult("Argument is null", new[] { "TxnId" }));
             }
-            if (TxnId.Length != 48)
+            if (TxnId.Length != 32)
             {
                 results.Add(new ValidationResult("Range exeption", new[] { "TxnId" }));
             }
@@ -48,17 +50,17 @@ namespace CYPCore.Models
             {
                 results.Add(new ValidationResult("Range exeption", new[] { "Mix" }));
             }
-            if (Mix > 22)
+            if (Mix != 22)
             {
                 results.Add(new ValidationResult("Range exeption", new[] { "Mix" }));
             }
             if (Rct == null)
             {
-                results.Add(new ValidationResult("Argument is null", new[] { "RingSig" }));
+                results.Add(new ValidationResult("Argument is null", new[] { "Rct" }));
             }
             if (Ver != 0x1)
             {
-                results.Add(new ValidationResult("Incorrect number", new[] { "Version" }));
+                results.Add(new ValidationResult("Incorrect number", new[] { "Ver" }));
             }
             if (Vin == null)
             {
@@ -71,22 +73,117 @@ namespace CYPCore.Models
 
             foreach (var bp in Bp)
             {
-
+                if (bp.Proof == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Bp.Proof" }));
+                }
+                if (bp.Proof.Length != 675)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Bp.Proof" }));
+                }
             }
 
             foreach (var vi in Vin)
             {
-
+                if (vi.Key.K_Image == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Vin.Key.K_Image" }));
+                }
+                if (vi.Key.K_Image.Length != 33)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Vin.Key.K_Image" }));
+                }
+                if (vi.Key.K_Offsets == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Vin.Key.K_Offsets" }));
+                }
+                if (vi.Key.K_Offsets.Length != 1452)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Vin.Key.K_Offsets" }));
+                }
             }
 
             foreach (var vo in Vout)
             {
-
+                if (vo.C == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Vout.C" }));
+                }
+                if (vo.C.Length != 33)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Vout.C" }));
+                }
+                if (vo.E == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Vout.E" }));
+                }
+                if (vo.E.Length != 33)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Vout.E" }));
+                }
+                if (vo.N == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Vout.N" }));
+                }
+                if (vo.N.Length > 241)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Vout.N" }));
+                }
+                if (vo.P == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Vout.P" }));
+                }
+                if (vo.P.Length != 33)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Vout.P" }));
+                }
+                if (!string.IsNullOrEmpty(vo.S))
+                {
+                    if (vo.S.Length != 16)
+                    {
+                        results.Add(new ValidationResult("Range exeption", new[] { "Vout.S" }));
+                    }
+                }
+                if (vo.T != CoinType.Coin && vo.T != CoinType.Coinbase && vo.T != CoinType.Coinstake && vo.T != CoinType.fee)
+                {
+                    results.Add(new ValidationResult("Argument exeption", new[] { "Vout.T" }));
+                }
             }
 
             foreach (var rct in Rct)
             {
-
+                if (rct.I == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Rct.I" }));
+                }
+                if (rct.I.Length != 32)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Rct.I" }));
+                }
+                if (rct.M == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Rct.M" }));
+                }
+                if (rct.M.Length != 1452)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Rct.M" }));
+                }
+                if (rct.P == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Rct.P" }));
+                }
+                if (rct.P.Length != 32)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Rct.P" }));
+                }
+                if (rct.S == null)
+                {
+                    results.Add(new ValidationResult("Argument is null", new[] { "Rct.S" }));
+                }
+                if (rct.S.Length != 1408)
+                {
+                    results.Add(new ValidationResult("Range exeption", new[] { "Rct.S" }));
+                }
             }
 
             return results;
