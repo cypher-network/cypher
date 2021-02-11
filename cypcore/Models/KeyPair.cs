e9e03da68428b4b7df2362afd7ef3db0be77a68a
pingpongsneak <matthew.hellyer@pm.me>
2021-02-11 01:28:51 +0000

    Fix(temp)!: issue locking data into RAM

--- cypcore/Models/KeyPair.cs
+++ cypcore/Models/KeyPair.cs
@@ -9,29 +9,49 @@ namespace CYPCore.Models
 {
     public class KeyPair : IDisposable
     {
-        private readonly SecureArray<byte> _privateKey;
+        public byte[] PrivateKey { get; private set; }
+        public byte[] PublicKey { get; }
 
         public KeyPair(byte[] privateKey, byte[] publicKey)
         {
             if (privateKey.Length % 16 != 0)
                 throw new ArgumentOutOfRangeException("Private Key length must be a multiple of 16 bytes.");
 
+            PrivateKey = privateKey;
             PublicKey = publicKey;
-            _privateKey = new SecureArray<byte>(32, SecureArrayType.ZeroedPinnedAndNoSwap);
-
-            Array.Copy(privateKey, _privateKey.Buffer, privateKey.Length);
-            Array.Clear(privateKey, 0, 32);
         }
 
-        ~KeyPair()
+        public void Dispose()
         {
-            Dispose();
+            Array.Clear(PrivateKey, 0, 32);
         }
-
-        public byte[] PublicKey { get; }
-
-        public byte[] PrivateKey => _privateKey.Buffer;
-
-        public void Dispose() => _privateKey?.Dispose();
     }
+
+    //public class KeyPair : IDisposable
+    //{
+    //    private readonly SecureArray<byte> _privateKey;
+
+    //    public KeyPair(byte[] privateKey, byte[] publicKey)
+    //    {
+    //        if (privateKey.Length % 16 != 0)
+    //            throw new ArgumentOutOfRangeException("Private Key length must be a multiple of 16 bytes.");
+
+    //        PublicKey = publicKey;
+    //        _privateKey = new SecureArray<byte>(32);
+
+    //        Array.Copy(privateKey, _privateKey.Buffer, privateKey.Length);
+    //        Array.Clear(privateKey, 0, 32);
+    //    }
+
+    //    ~KeyPair()
+    //    {
+    //        Dispose();
+    //    }
+
+    //    public byte[] PublicKey { get; }
+
+    //    public byte[] PrivateKey => _privateKey.Buffer;
+
+    //    public void Dispose() => _privateKey?.Dispose();
+    //}
 }
