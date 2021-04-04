0627e011b5fb791407416b5197e74a4dca97bccf
pingpongsneak <matthew.hellyer@pm.me>
2021-04-04 23:03:25 +0100

    refactor!: moving interface

--- cypcore/Persistence/DeliveredRepository.cs
+++ cypcore/Persistence/DeliveredRepository.cs
@@ -1,29 +1,19 @@
 ﻿// CYPCore by Matthew Hellyer is licensed under CC BY-NC-ND 4.0.
 // To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-nd/4.0
 
-using System;
-
-using CYPCore.Extensions;
 using Serilog;
-using Stratis.Patricia;
-
-using CYPCore.Extentions;
 using CYPCore.Models;
 
 namespace CYPCore.Persistence
 {
     public interface IDeliveredRepository : IRepository<BlockHeaderProto>
     {
-        BlockHeaderProto ToTrie(BlockHeaderProto blockHeader);
-        byte[] MerkleRoot { get; }
-        void ResetTrie();
     }
 
     public class DeliveredRepository : Repository<BlockHeaderProto>, IDeliveredRepository
     {
         private readonly IStoreDb _storeDb;
         private readonly ILogger _logger;
-        private readonly IPatriciaTrie _stateTrie;
 
         public DeliveredRepository(IStoreDb storeDb, ILogger logger)
             : base(storeDb, logger)
@@ -31,54 +21,7 @@ namespace CYPCore.Persistence
             _storeDb = storeDb;
             _logger = logger.ForContext("SourceContext", nameof(DeliveredRepository));
 
-            _stateTrie = new PatriciaTrie();
-
             SetTableName(StoreDb.DeliveredTable.ToString());
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public byte[] MerkleRoot => _stateTrie.GetRootHash();
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public void CommitTrie()
-        {
-            _stateTrie.Flush();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="blockHeader"></param>
-        /// <returns></returns>
-        public BlockHeaderProto ToTrie(BlockHeaderProto blockHeader)
-        {
-            try
-            {
-                var leftright = blockHeader.ToHash();
-                _stateTrie.Put(leftright, leftright);
-                _stateTrie.Flush();
-
-                blockHeader.MerkelRoot = MerkleRoot.ByteToHex();
-            }
-            catch (System.Exception ex)
-            {
-                blockHeader = null;
-                _logger.Here().Error(ex, "Error while adding to trie");
-            }
-
-            return blockHeader;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public void ResetTrie()
-        {
-            _stateTrie.SetRootHash(Array.Empty<byte>());
         }
     }
 }
