fc487112fafcd163a62701b7f2d7ce2d42db9301
pingpongsneak <matthew.hellyer@pm.me>
2021-04-04 23:15:50 +0100

    feat: staking block winner model

---
+++ cypcore/Models/BlockWinner.cs
@@ -0,0 +1,11 @@
+// CYPCore by Matthew Hellyer is licensed under CC BY-NC-ND 4.0.
+// To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-nd/4.0
+
+namespace CYPCore.Models
+{
+    public class BlockWinner
+    {
+        public BlockHeaderProto BlockHeader { get; set; }
+        public long Finish { get; set; }
+    }
+}
