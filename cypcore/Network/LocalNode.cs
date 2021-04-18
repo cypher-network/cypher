83dbfb5b5557826e7942168da57d64d2ad494747
pingpongsneak <matthew.hellyer@pm.me>
2021-04-18 12:48:18 +0100

    refactor: relocating http calls

--- cypcore/Network/LocalNode.cs
+++ cypcore/Network/LocalNode.cs
@@ -4,16 +4,12 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Net.Http;
 using System.Collections.Generic;
-using System.Reactive.Linq;
 using Dawn;
 using Serilog;
 using CYPCore.Extensions;
 using CYPCore.Serf;
 using CYPCore.Models;
-using CYPCore.Persistence;
-using Rx.Http;
 
 namespace CYPCore.Network
 {
@@ -28,9 +24,8 @@ namespace CYPCore.Network
         void Ready();
         Task Leave();
         Task JoinSeedNodes();
-        IObservable<Peer> ObservePeers();
         Task<ulong[]> Nodes();
-        IObservable<NetworkBlockHeight> ObservePeerBlockHeight(Peer peer);
+        public ISerfClient SerfClient { get; }
     }
 
     /// <summary>
@@ -39,23 +34,23 @@ namespace CYPCore.Network
     public class LocalNode : ILocalNode
     {
         private readonly ISerfClient _serfClient;
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly NetworkClient _networkClient;
         private readonly ILogger _logger;
-
         private TcpSession _tcpSession;
 
-        public LocalNode(ISerfClient serfClient, IUnitOfWork unitOfWork, ILogger logger)
+        public LocalNode(ISerfClient serfClient, NetworkClient networkClient, ILogger logger)
         {
             _serfClient = serfClient;
-            _unitOfWork = unitOfWork;
+            _networkClient = networkClient;
             _logger = logger.ForContext("SourceContext", nameof(LocalNode));
+            SerfClient = _serfClient;
         }
 
         public void Ready()
         {
             _tcpSession = _serfClient.TcpSessionsAddOrUpdate(
-                new TcpSession(_serfClient.SerfConfigurationOptions.Listening)
-                    .Connect(_serfClient.SerfConfigurationOptions.RPC));
+                new TcpSession(_serfClient.SerfConfigurationOptions.Listening).Connect(_serfClient
+                    .SerfConfigurationOptions.RPC));
         }
 
         /// <summary>
@@ -64,15 +59,10 @@ namespace CYPCore.Network
         /// <param name="topicType"></param>
         /// <param name="data"></param>
         /// <returns></returns>
-        public Task Broadcast(TopicType topicType, byte[] data)
+        public async Task Broadcast(TopicType topicType, byte[] data)
         {
-            var localPeers = ObservePeers().Select(peer => peer).ToArray();
-            localPeers.Subscribe(async peers =>
-            {
-                await Broadcast(peers, topicType, data);
-            });
-
-            return Task.CompletedTask;
+            var peers = await GetPeers();
+            await Broadcast(peers.Values.ToArray(), topicType, data);
         }
 
         /// <summary>
@@ -86,18 +76,15 @@ namespace CYPCore.Network
         {
             Guard.Argument(data, nameof(data)).NotNull();
             Guard.Argument(peers, nameof(peers)).NotNull();
-
             _logger.Here().Information("Broadcasting {@TopicType} to nodes {@Nodes}", topicType, peers);
-
             try
             {
-                var broadcastPeers = peers.Select(p => p).ToList();
-                if (broadcastPeers.Any())
+                if (peers.Any())
                 {
-                    var broadcast = new Broadcast(_logger, 4);
-                    foreach (var t in broadcastPeers)
+                    foreach (var peer in peers)
                     {
-                        Task.Run(async () => { await broadcast.Send(data, topicType, t.Host); }).ConfigureAwait(false);
+                        Task.Run(async () => { await _networkClient.SendAsync(data, topicType, peer.Host); })
+                            .ConfigureAwait(false);
                     }
                 }
             }
@@ -113,25 +100,11 @@ namespace CYPCore.Network
         /// 
         /// </summary>
         /// <returns></returns>
-        public IObservable<Peer> ObservePeers()
-        {
-            return Observable.Defer(async () =>
-            {
-                var peers = await GetPeers();
-                return peers.Values.ToObservable();
-            });
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
         public async Task<ulong[]> Nodes()
         {
             var peers = await GetPeers();
             if (peers == null) return null;
-
-            var totalNodes = (ulong)peers.Count;
+            var totalNodes = (ulong) peers.Count;
             return totalNodes != 0 ? peers.Keys.ToArray() : null;
         }
 
@@ -142,7 +115,6 @@ namespace CYPCore.Network
         public async Task<Dictionary<ulong, Peer>> GetPeers()
         {
             var peers = new Dictionary<ulong, Peer>();
-
             if (_tcpSession == null)
             {
                 Ready();
@@ -150,7 +122,6 @@ namespace CYPCore.Network
 
             var tcpSession = _serfClient.GetTcpSession(_tcpSession.SessionId);
             _ = _serfClient.Connect(tcpSession.SessionId);
-
             if (!tcpSession.Ready)
             {
                 _logger.Here().Error("Serf client failed to connect");
@@ -159,17 +130,16 @@ namespace CYPCore.Network
 
             var membersResult = await _serfClient.Members(tcpSession.SessionId);
             var members = membersResult.Value.Members.ToList();
-
-            foreach (var member in members.Where(member =>
-                _serfClient.Name != member.Name && member.Status == "alive"))
+            foreach (var member in members.Where(member => _serfClient.Name != member.Name && member.Status == "alive"))
             {
                 if (_serfClient.ClientId == Helper.Util.HashToId(member.Tags["pubkey"])) continue;
-
                 member.Tags.TryGetValue("rest", out var restEndpoint);
-
                 if (string.IsNullOrEmpty(restEndpoint)) continue;
-
                 if (!Uri.TryCreate($"{restEndpoint}", UriKind.Absolute, out var uri)) continue;
+                if (uri.Host is "0.0.0.0" or "::0")
+                {
+                    continue;
+                }
 
                 var peer = new Peer
                 {
@@ -178,44 +148,15 @@ namespace CYPCore.Network
                     PublicKey = member.Tags["pubkey"],
                     NodeName = member.Name
                 };
-
                 if (peers.ContainsKey(peer.ClientId)) continue;
                 if (peers.TryAdd(peer.ClientId, peer)) continue;
-
-                _logger.Here().Error("Failed adding or exists in remote nodes: {@Node}",
-                    member.Name);
+                _logger.Here().Error("Failed adding or exists in remote nodes: {@Node}", member.Name);
             }
 
             return peers;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="peer"></param>
-        /// <returns></returns>
-        public IObservable<NetworkBlockHeight> ObservePeerBlockHeight(Peer peer)
-        {
-            return Observable.Create<NetworkBlockHeight>(observer =>
-            {
-                var client = new HttpClient { Timeout = TimeSpan.FromSeconds(15) };
-                var http = new RxHttpClient(client, null);
-                http.Get<BlockHeight>($"{peer.Host}/chain/height")
-                    .Subscribe(async x =>
-                    {
-                        var networkBlockHeight = new NetworkBlockHeight
-                        {
-                            Local = new BlockHeight { Height = await _unitOfWork.HashChainRepository.CountAsync() },
-                            Remote = x
-                        };
-
-                        observer.OnNext(networkBlockHeight);
-
-                    }, observer.OnError);
-
-                return () => { };
-            });
-        }
+        public ISerfClient SerfClient { get; }
 
         /// <summary>
         /// 
@@ -230,7 +171,6 @@ namespace CYPCore.Network
 
             var tcpSession = _serfClient.GetTcpSession(_tcpSession.SessionId);
             _ = _serfClient.Connect(tcpSession.SessionId);
-
             if (!tcpSession.Ready)
             {
                 _logger.Here().Error("Serf client failed to connect");
@@ -253,7 +193,6 @@ namespace CYPCore.Network
 
             var tcpSession = _serfClient.GetTcpSession(_tcpSession.SessionId);
             _ = _serfClient.Connect(tcpSession.SessionId);
-
             if (!tcpSession.Ready)
             {
                 _logger.Here().Error("Serf client failed to connect");
